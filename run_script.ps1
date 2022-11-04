967a4f6d8ac651fbbd20b4992eab3b1abb7c611f
An777 <anux777@ukr.net>
2022-11-04 04:08:52 +0200

    Create PowerShell script and use him in Task Scheduler

---
+++ run_script.ps1
@@ -0,0 +1,7 @@
+#Run script Task Schedudle
+#Usage:PS C:Scripts> .run_script.ps1 arg1 arg2 arg3 arg4
+
+
+$data = Get-Date -format 'dd/MM/yyyy HH:mm:ss'
+
+$data + " " + $Args[1] + " " + $Args[3] >> C:\Users\Usr.def0ult.rdp\Documents\testlog.txt
