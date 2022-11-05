79ce98a12f59008a3ee31ce7118a5fb088727beb
An777 <anux777@ukr.net>
2022-11-05 04:08:47 +0200

    Add blocked IP address in new create rule

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Windows-RDP_SecurityCheckup
+
+ New-NetFirewallRule -DisplayName "BlockRDPBruteForce" -RemoteAddress 213.6.148.83 -Direction Inbound -Protocol TCP -LocalPort 3389 -Action Block
+ 
+  Get-NetFirewallPortFilter | Where-Object -Property LocalPort -EQ 3389
+  
+  
+   Get-NetFirewallRule -DisplayName "BlockRDPBruteForce"
+   
+   
--- run_script.ps1
+++ run_script.ps1
@@ -1,7 +1,21 @@
 #Run script Task Schedudle
 #Usage:PS C:Scripts> .run_script.ps1 arg1 arg2 arg3 arg4
 
-
+$my_username = "An777"
 $data = Get-Date -format 'dd/MM/yyyy HH:mm:ss'
 
 $data + " " + $Args[1] + " " + $Args[3] >> testlog.txt
+
+$username = $Args[1]
+$ip_address = $Args[3]
+
+
+$current_ips = (Get-NetFirewallRule -DisplayName "BlockRDPBruteForce" | Get-NetFirewallAddressFilter ).RemoteAddress
+
+$current_ips += $ip_address
+
+if ($username -eq $my_username) {
+	write-host("It is me")
+} else {
+	Set-NetFirewallRule -DisplayName "BlockRDPBruteForce" -RemoteAddress $current_ips
+}
