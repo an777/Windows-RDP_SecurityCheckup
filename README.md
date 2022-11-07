93a7ea0ce3d069435053bd019c877c6b4a3c28c4
An777 <anux777@ukr.net>
2022-11-07 03:39:26 +0200

    complite write ver 1.0 beta and write comment

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Windows-RDP_SecurityCheckup
 
+http://woshub.com/block-rdp-brute-force-powershell-firewall-rules/
+
  New-NetFirewallRule -DisplayName "BlockRDPBruteForce" -RemoteAddress 213.6.148.83 -Direction Inbound -Protocol TCP -LocalPort 3389 -Action Block
  
   Get-NetFirewallPortFilter | Where-Object -Property LocalPort -EQ 3389
--- run_script.ps1
+++ run_script.ps1
@@ -1,21 +1,36 @@
 #Run script Task Schedudle
 #Usage:PS C:Scripts> .run_script.ps1 arg1 arg2 arg3 arg4
 
-$my_username = "An777"
-$data = Get-Date -format 'dd/MM/yyyy HH:mm:ss'
-
-$data + " " + $Args[1] + " " + $Args[3] >> testlog.txt
-
+#Uses args
 $username = $Args[1]
 $ip_address = $Args[3]
 
 
-$current_ips = (Get-NetFirewallRule -DisplayName "BlockRDPBruteForce" | Get-NetFirewallAddressFilter ).RemoteAddress
+#A user who will not be blocked
+$my_username = "An777"
 
-$current_ips += $ip_address
+#datatime
+$data = Get-Date -format 'dd/MM/yyyy HH:mm:ss'
 
-if ($username -eq $my_username) {
-	write-host("It is me")
+
+#alredy blocked IP addresses
+$current_ips = [array](Get-NetFirewallRule -DisplayName "BlockRDPBruteForce" | Get-NetFirewallAddressFilter ).RemoteAddress
+
+#add new to array
+if ($ip_address -in $current_ips) {
+	$str = "Dublicate IP "
 } else {
+	$str="Add IP "
+	$current_ips += "$ip_address"
+}
+
+#Save to our rule
+if ($username -eq $my_username) {
+	$str +="It is me "
+} else {
+	$str +="to rule "
 	Set-NetFirewallRule -DisplayName "BlockRDPBruteForce" -RemoteAddress $current_ips
 }
+
+#write log
+$data + " " + $str + $username + " " + $ip_address >> testlog.txt
